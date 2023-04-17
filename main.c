57dc310eb18a7d981a73cd836a7a7d73f216ddb2
Shinji Ikari <shinjiikari@BalthasarMagi-2.net>
2023-04-16 21:40:40 -0800

    Return sequence now works (using int not char)

--- main.c
+++ main.c
@@ -38,6 +38,70 @@ int main(){
 }
 
 
+void getCommand(char *buffer, int inputLength){	//STUB
+
+	/*
+	for(int = 0; i < sizeof(inputLength); i++)
+		if(strcmp (buffer, commandList[0])){
+				
+		}
+	*/	
+}
+
+//--------------------
+//can take a char from any source
+void processInput(char input){
+	inputToBuffer(input);
+	if(input == 13){ 
+		getCommand(input_buffer, top_of_buffer);
+		flushBuffer();
+		printf("\n");
+	}		
+}
+//-----------------
+void inputToBuffer(char input_char){
+	//if(input_char > 256) //doesn't take any non-asccii characters
+	//	return;
+	if(top_of_buffer >= sizeof(input_buffer)){
+		flushBuffer();
+		printf("buffer overflow \n");	
+	}
+		
+	
+	printf("%c", input_char);
+	input_buffer[top_of_buffer] = input_char;
+	top_of_buffer++;	
+}
+//-------------
+
+void flushBuffer(){
+	top_of_buffer = 0;
+}
+
+
+void turnOn()
+{
+	gpio_put(25, 1); // Set pin 25 to high
+	printf("LED ON!\n");
+	
+}
+void turnOff()
+{
+	gpio_put(25, 0); // Set pin 25 to high
+	printf("LED OFF\n");
+	
+}
+/*
+gpio_put(25, 1); // Set pin 25 to high
+printf("LED ON!\n");
+sleep_ms(1000); // 0.5s delay
+
+gpio_put(25, 0); // Set pin 25 to low
+printf("LED OFF!\n");
+sleep_ms(1000); // 0.5s delay
+*/
+
+
 /*
 struct _commmand_{
 	char *name;
@@ -65,69 +129,3 @@ struct _command_ *commandList[]=
 	};
 */
 
-
-
-
-void getCommand(char *buffer, int inputLength){	//STUB
-
-	/*
-	for(int = 0; i < sizeof(inputLength); i++)
-		if(strcmp (buffer, commandList[0])){
-				
-		}
-	*/	
-}
-
-
-//--------------------
-//can take a char from any source
-void processInput(char input){
-	inputToBuffer(input);
-	if(input == '\n'){ 
-		getCommand(input_buffer, top_of_buffer);
-		flushBuffer();
-		printf('\n');
-	}		
-}
-//-----------------
-void inputToBuffer(char input_char){
-	//if(input_char > 256) //doesn't take any non-asccii characters
-	//	return;
-	if(top_of_buffer >= sizeof(input_buffer)){
-		flushBuffer();
-		printf("buffer overflow \n");	
-	}
-		
-	
-	printf("%c", input_char);
-	input_buffer[top_of_buffer] = input_char;
-	top_of_buffer++;	
-}
-//-------------
-void flushBuffer(){
-	top_of_buffer = 0;
-	
-}
-
-
-void turnOn()
-{
-	gpio_put(25, 1); // Set pin 25 to high
-	printf("LED ON!\n");
-	
-}
-void turnOff()
-{
-	gpio_put(25, 0); // Set pin 25 to high
-	printf("LED OFF\n");
-	
-}
-/*
-gpio_put(25, 1); // Set pin 25 to high
-printf("LED ON!\n");
-sleep_ms(1000); // 0.5s delay
-
-gpio_put(25, 0); // Set pin 25 to low
-printf("LED OFF!\n");
-sleep_ms(1000); // 0.5s delay
-*/
